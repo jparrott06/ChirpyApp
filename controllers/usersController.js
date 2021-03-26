eda99c00fe78c1cae6cc37b6c2d832788b11da1a
jparrott06 <48459550+jparrott06@users.noreply.github.com>
2021-03-26 17:21:44 -0600

    signup server-side validation complete and tested. I added back onsubmit="return ValidateForm(this)" as well. If we want to test server-side we'll have to remove again

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -18,8 +18,44 @@ exports.getAllUsers = (req, res) => {
 exports.saveUser = (req, res) => {
     console.log(req.body);
 
+    var error = new Object();
+    error.fields = [];
+    error.required = "Error: Required field cannot be blank.";
+    //error.passmsg = "Error: Passwords do not match.";
 
-    //var passerror = "";
+    var required_field = new Map([["FirstName", req.body.FirstName], ["LastName", req.body.LastName], ["Username", req.body.Username], 
+    ["Email", req.body.Email], ["pass1", req.body.pass1], ["pass2", req.body.pass2],
+    ["txtDoB", req.body.txtDoB], ["ddSecurityQuestion", req.body.ddSecurityQuestion], ["txtAnswer", req.body.txtAnswer]]);
+
+    for (const [key, value] of required_field.entries()) {
+        if(value == "") {
+            console.log(key + " is " + value);
+            error.fields.push(key);
+        }
+      }
+
+    //(1) Check that all required fields are filled
+
+    // if(req.body.Email == "") {
+    //     error.fields.push("Email");
+    //     console.log(JSON.stringify(error));
+    //     console.log(error.fields.includes("Email"));
+    //     console.log(error.fields);
+    //     res.render("signup", {error: error});
+    // }
+
+
+    //(2) Check that passwords match
+
+    if(req.body.pass1 != req.body.pass2) {
+        console.log("Passwords do not match");
+        error.passmsg = "Error: Passwords do not match.";
+            // res.render("signup", {error: error});
+    }
+
+    if(error.passmsg != undefined || error.fields.length > 0) {
+        res.render("signup", {error: error});
+    }
 
     let newUser = new User({
         FirstName: req.body.FirstName,
@@ -42,7 +78,7 @@ exports.saveUser = (req, res) => {
 }
 
 exports.getSignupPage = (req, res) => {
-    res.render("signup");
+    res.render("signup", {error: false});
 }
 
 exports.getSigninPage = (req, res) => {

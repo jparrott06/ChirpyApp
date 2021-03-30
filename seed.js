b6906b3bf5aacf95a0508165ad5990c9740a70c6
jparrott06 <jparrott06@hotmail.com>
2021-03-30 15:02:20 -0600

    fixed seed.js - useUnifiedTopology

--- seed.js
+++ seed.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose"),
 
 mongoose.connect(
   "mongodb://localhost:27017/chirpy_app",
-  { useNewUrlParser: true }
+  { useUnifiedTopology: true, useNewUrlParser: true }
 );
 mongoose.set("useCreateIndex", true);
 mongoose.connection;

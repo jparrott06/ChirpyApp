dde9e51184d072b19357a72ee1d2b79a6e4155d1
jparrott06 <jparrott06@hotmail.com>
2021-04-11 19:05:40 -0600

    refactored user model: timestamps, virtualMethod, added passport-local-mongoose code for when we move over to using it in lieu of password.

--- models/user.js
+++ models/user.js
@@ -1,16 +1,34 @@
+"use strict";
+//const passportLocalMongoose = require("passport-local-mongoose");
+
 const mongoose = require("mongoose"),
-    userSchema = mongoose.Schema({
-        FirstName: { type: String, required: true },
-        LastName: { type: String, required: true },
-        Username: { type: String, required: true, index: { unique: true }},
-        Gender: { type: String },
-        Location: { type: String },
-        Email: { type: String, required: true, index: {unique: true} },
-        Password: { type: String, required: true },
-        DoB: { type: Date, required: true },
-        SecurityQuestion: { type: String, required: true},
-        Answer: { type: String, required: true},
-        Bio: { type: String, maxlength: 250}
-    });
+    { Schema } = require("mongoose"),
+    userSchema = new Schema(
+        {
+            FirstName: { type: String, required: true },
+            LastName: { type: String, required: true },
+            Username: { type: String, required: true, index: { unique: true } },
+            Gender: { type: String },
+            Location: { type: String },
+            Email: { type: String, required: true, index: { unique: true } },
+            Password: { type: String, required: true },
+            DoB: { type: Date, required: true },
+            SecurityQuestion: { type: String, required: true },
+            Answer: { type: String, required: true },
+            Bio: { type: String, maxlength: 250 }
+        },
+        {
+            timestamps: true
+        }
+
+    )
+
+userSchema.virtual("fullName").get(function () {
+    return `${this.FirstName} ${this.LastName}`;
+});
+
+// userSchema.plugin(passportLocalMongoose, {
+//     usernameField: "Email"
+// });
 
 module.exports = mongoose.model("User", userSchema);

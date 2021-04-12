1bba6ce14471107001f2770a8e7fadb9944a9633
jparrott06 <jparrott06@hotmail.com>
2021-04-11 19:45:29 -0600

    added boilerplate chirpsController based on last classwork: Will modify as needed. TODO: Probably want to have user model contain chirps. Will investigate and update models.

---
+++ controllers/chirpsController.js
@@ -0,0 +1,115 @@
+"use strict";
+
+const Chirp = require("../models/chirp"),
+    getChirpParams = body => {
+        return {
+            chirpBody: body.chirpBody,
+            user: body.user
+        }
+    }
+
+module.exports = {
+    index: (req, res, next) => {
+        Chirp.find({})
+            .then(chirps => {
+                res.locals.chirps = chirps;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching chirps: ${error.message}`);
+                next(error);
+            });
+    },
+    indexView: (req, res) => {
+        if (req.query.format === "json") {
+            res.json(res.locals.chirps);
+        } else {
+            res.render("chirps/index");
+        }
+    },
+    new: (req, res) => {
+        res.render("chirps/new");
+    },
+
+    create: (req, res, next) => {
+        let chirpParams = getChirpParams(req.body);
+        Chirp.create(chirpParams)
+            .then(chirp => {
+                res.locals.redirect = "/chirps";
+                res.locals.chirp = chirp;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error saving chirp: ${error.message}`);
+                next(error);
+            });
+    },
+
+    show: (req, res, next) => {
+        let chirpId = req.params.id;
+        Chirp.findById(chirpId)
+            .then(chirp => {
+                res.locals.chirp = chirp;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching chirp by ID: ${error.message}`);
+                next(error);
+            });
+    },
+
+    showView: (req, res) => {
+        res.render("chirps/show");
+    },
+
+    edit: (req, res, next) => {
+        let chirpId = req.params.id;
+        Chirp.findById(chirpId)
+            .then(chirp => {
+                res.render("chirps/edit", {
+                    chirp: chirp
+                });
+            })
+            .catch(error => {
+                console.log(`Error fetching chirp by ID: ${error.message}`);
+                next(error);
+            });
+    },
+
+    update: (req, res, next) => {
+        let chirpId = req.params.id,
+        chirpParams = getChirpParams(req.body);
+
+        Chirp.findByIdAndUpdate(chirpId, {
+            $set: chirpParams
+        })
+            .then(chirp => {
+                res.locals.redirect = `/chirps/${chirpId}`;
+                res.locals.chirp = chirp;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error updating chirp by ID: ${error.message}`);
+                next(error);
+            });
+    },
+
+    delete: (req, res, next) => {
+        let chirpId = req.params.id;
+        Chirp.findByIdAndRemove(chirpId)
+            .then(() => {
+                res.locals.redirect = "/chirps";
+                next();
+            })
+            .catch(error => {
+                console.log(`Error deleting chirp by ID: ${error.message}`);
+                next();
+            });
+    },
+
+    redirectView: (req, res, next) => {
+        let redirectPath = res.locals.redirect;
+        if (redirectPath !== undefined) res.redirect(redirectPath);
+        else next();
+    }
+};

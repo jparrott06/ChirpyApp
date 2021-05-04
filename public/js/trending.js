cbe7d85a921b26790f64a411c853b9b7dc52b40a
jparrott06 <48459550+jparrott06@users.noreply.github.com>
2021-05-03 21:13:59 -0600

    Fixed modal for trending hashtags  - got rid of commented out code - uploaded same bootstrap, js and jquery we used in class

--- public/js/trending.js
+++ public/js/trending.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     $(".modal-button").click(event => {
         let $button = $(event.target),
-        hashtag = $button.data("id");
+            hashtag = $button.data("id");
         console.log(hashtag);
         $(".modal-body").html("");
         $.get(`/api/chirps/hashtag/${hashtag}`, (results = {}) => {
@@ -10,17 +10,35 @@ $(document).ready(() => {
             if (!data || !data.hashChirps) return;
             data.hashChirps.forEach((hashChirp) => {
                 $(".modal-body").append(
-                    `<div>
-                    <span class="course-title">
-                    @${hashChirp.userInfo.Username}
-                    </span>
-                    <span class="course-cost">${hashChirp.userInfo.FirstName} ${hashChirp.userInfo.LastName}</span>
-                    <div class="course-description">
-                    ${hashChirp.chirpBody}
+                    `<div class="post">
+                    <div class="post__body">
+                    <div class="post__header">
+                    <div class="post__headerText">
+                    <h3> ${hashChirp.userInfo.FirstName} ${hashChirp.userInfo.LastName} 
+                    <span class="post__headerSpecial">@${hashChirp.userInfo.Username}</span>
+                    </h3>
+                    </div>
+                    <div class="post__headerDescription">
+                    <center>
+                    <p>${hashChirp.chirpBody}</p>
+                    </center>
+                    </div>
+                    </div>
                     </div>
                     </div>`
                 );
             });
         });
-        });
     });
+});
+
+
+//   `<div>
+//                     <span class="course-title">
+//                     @${hashChirp.userInfo.Username}
+//                     </span>
+//                     <span class="course-cost">${hashChirp.userInfo.FirstName} ${hashChirp.userInfo.LastName}</span>
+//                     <div class="course-description">
+//                     ${hashChirp.chirpBody}
+//                     </div>
+//                     </div>`

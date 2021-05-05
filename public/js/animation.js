c46b942f45fd9ae20a6bd8f4a226b786a2c40d0c
jparrott06 <jparrott06@hotmail.com>
2021-05-04 20:35:27 -0600

    made animation for homepage

---
+++ public/js/animation.js
@@ -0,0 +1,16 @@
+var id = null;
+function myMove() {
+  var elem = document.getElementById("home-icon");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.animationDirection = right;
+    }
+  }
+}

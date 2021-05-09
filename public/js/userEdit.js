e850a7ed22771ccc7fa4200063781b79594b927d
jparrott06 <jparrott06@hotmail.com>
2021-05-09 12:59:01 -0600

    just going to display the security answer for edit now. doesn't make sense for this page.

--- public/js/userEdit.js
+++ public/js/userEdit.js
@@ -1,23 +1,23 @@
-function func() {
-    // $("#ddSecurityQuestion option:last").attr("selected", "selected"); 
-    $(document).ready(function () {
-        $('#ddSecurityQuestion').on('change', function () {
-            if (this.value == 'What was your high school mascot?') {
-                $("#txtAnswer").show();
-            }
-            else if (this.value == 'Who is your favorite author?') {
-                $("#txtAnswer").show();
-            }
-            else if (this.value == "What is your mother's maiden name?") {
-                $("#txtAnswer").show();
-            }
-            else {
-                $("#txtAnswer").hide();
-            }
+// function func() {
+//     // $("#ddSecurityQuestion option:last").attr("selected", "selected"); 
+//     $(document).ready(function () {
+//         $('#ddSecurityQuestion').on('change', function () {
+//             if (this.value == 'What was your high school mascot?') {
+//                 $("#txtAnswer").show();
+//             }
+//             else if (this.value == 'Who is your favorite author?') {
+//                 $("#txtAnswer").show();
+//             }
+//             else if (this.value == "What is your mother's maiden name?") {
+//                 $("#txtAnswer").show();
+//             }
+//             else {
+//                 $("#txtAnswer").hide();
+//             }
 
-        });
-    });
-}
+//         });
+//     });
+// }
 // function test()
 // {
 //     $("#GenderQuestion option:last").attr("selected", "selected"); 
